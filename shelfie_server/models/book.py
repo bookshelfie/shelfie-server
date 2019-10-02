feb91a278c4857623ff693c0a6300ad72febc9f2
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-02 09:40:05 +0530

    update books table definition

--- shelfie_server/models/book.py
+++ shelfie_server/models/book.py
@@ -20,14 +20,18 @@ class Book(db.Model):
     isbn = db.Column(db.String(25), nullable=True)
     title = db.Column(db.String(255), nullable=False)
     # TODO: move author(s) to external table.
+    series = db.Column(db.String(255))
     author = db.Column(db.String(255), nullable=False)
     publisher = db.Column(db.String(255), nullable=True)
-    # TODO: add genre, format
+    # TODO: add genre
+    _format = db.Column("format", db.String(255), default="Paperback")
     width = db.Column(db.Float, default=1.0)
     height = db.Column(db.Float, default=1.0)
     # shelves go from A to Z
     shelf = db.Column(db.String(10), nullable=True)
     # a slot entry will be in the m:n format.
     slots = db.Column(db.String(10), nullable=True)
+    sold = db.Column(db.BOOLEAN, default=False)
+    comment = db.Column(db.TEXT)
     created_on = db.Column(db.TIMESTAMP, default=datetime.datetime.now)
     updated_at = db.Column(db.TIMESTAMP, onupdate=datetime.datetime.now)
