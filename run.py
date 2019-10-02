96e8db81a1afbbdd27ae904ad0d4304f994e29db
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-02 09:40:16 +0530

    rename run.py

--- run.py
+++
@@ -1,3 +0,0 @@
-from shelfie_server import create_app
-
-app = create_app()
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""wsgi file
+Use with gunicorn
+
+gunicorn -w 1 --bind 0.0.0.0:14001 wsgi
+
+"""
+from shelfie_server import create_app
+
+application = create_app() # pylint: ignore=invalid-name
+
+
+if __name__=="__main__":
+    application.run()
