1166786531cbf9d7b74cdd7533b6343ba42573b9
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-07 21:56:38 +0530

    disable unnecessary queue subscription for now.

--- shelfie_server/extensions/mqtt.py
+++ shelfie_server/extensions/mqtt.py
@@ -3,29 +3,31 @@
 
 """MQTT definition"""
 
-
+from flask import current_app
 from flask_mqtt import Mqtt
 
 mqtt = Mqtt()
+
 
 @mqtt.on_subscribe()
 def handle_subscribe(*args, **kwargs):
     """Handle subscription"""
     pass
 
+
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
     """Subscribe to topic on connect"""
-    mqtt.subscribe("server/update")
-
+    #mqtt.subscribe("server/update")
+    pass
 
 @mqtt.on_message()
 def handle_message(client, userdata, message):
     """Handle individual messages"""
-    data = dict(topic=message.topic, payload=message.payload.decode())
-
+    #data = dict(topic=message.topic, payload=message.payload.decode())
+    pass
 
 @mqtt.on_publish()
 def on_publish(client, userdata, mid):
     """jandle event on publishing to a topic"""
-    print("{}, {}, {} being published.".format(client, userdata, mid))
+    current_app.logger.info("{}, {}, {} being published.".format(client, userdata, mid))
