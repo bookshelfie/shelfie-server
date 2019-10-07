d41e66dc04dbf7ebb25f98350444256a536acbb6
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-07 21:29:00 +0530

    bind logger to gunicorn's logger

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,13 @@ gunicorn -w 1 --bind 0.0.0.0:14001 wsgi
 """
 from shelfie_server import create_app
 
-application = create_app() # pylint: disable=invalid-name
-
 
 if __name__ == "__main__":
-    application.run()
+    app = create_app() # pylint: disable=invalid-name
+    app.run()
+else:
+    import logging
+    application = create_app() # pylint: disable=invalid-name
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    application.logger.handlers = gunicorn_logger.handlers
+    application.logger.setLevel(gunicorn_logger.level)
