c3ccbf8f19ab8c596a4b618b3d5e4f28b70e284b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-07 18:23:34 +0530

    add pylint disable

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,8 @@ gunicorn -w 1 --bind 0.0.0.0:14001 wsgi
 """
 from shelfie_server import create_app
 
-application = create_app() # pylint: ignore=invalid-name
+application = create_app() # pylint: disable=invalid-name
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     application.run()
